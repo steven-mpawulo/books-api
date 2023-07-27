9ffafec50764fe369836e278947ca70f5b72f096
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-27 12:31:44 +0300

    creates list of book objects

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const express = require('express');
 const app = express();
 
+let books = [
+    {
+        id: 1,
+        title: "Forgiven",
+        author: "Ali",
+        publisher: "Stain Ltd"
+    },
+    {
+        id: 2,
+        title: "War",
+        author: "Zack",
+        publisher: "CRE Ltd"
+    },
+    {
+        id: 3,
+        title: "Happiness",
+        author: "Steve",
+        publisher: "Mock Ltd"
+    },
+    {
+        id: 4,
+        title: "Earth",
+        author: "Smith",
+        publisher: "Stain Ltd"
+    },
+    {
+        id: 5,
+        title: "Beyond",
+        author: "Henry",
+        publisher: "DR Ltd"
+    },
+];
+
 app.get('/', (req, res) => {
-    res.send('<p></p>')
+    res.send('Hello world')
 });
 
 const Port = 3001;
